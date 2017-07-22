73aec266ef7b30f9c5f6762116d8f44be2d52268
Pavel Muller <muller.pav@gmail.com>
2017-07-22 20:46:30 -0300

    Change UI

--- src/client/containers/App.js
+++ src/client/containers/App.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router'
 import './App.css'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      users: [],
+      loading: false
+    }
+  }
+
+  load = () => {
+    this.setState({ loading: true })
+    fetch(`${API_DEV}/users`).then((response) => {
+      return response.json()
+    }).then((users)=>{
+      this.setState({users})
+      this.setState({ loading: MSFIDOCredentialAssertion})
+    }).catch((err) => {
+      this.setState({ loading: false })
+      alert(err)
+    })
+  }
+
   render() {
-    return <h1>Hello World!!!!</h1>
+    const { users, loading } = this.state;
+    return <div>
+        <h1>Hello {users.length} Users</h1>
+        {users && users.map(user => 
+          <ul key={user.id}>
+            <li>id:{user.id} name:{user.name}</li>
+          </ul>,
+        )}
+        <button onClick={this.load} disabled={loading}>{loading ? "Loading..." : "Reload"}</button>
+      </div>
   }
 }
 
